9ead89626182da9bb1ba048f5484008f8038a9d6
Filippa Fast <filippa.fast@learnet.se>
2023-11-07 11:59:57 +0100

    Added Filippa Unittests

---
+++ UnitTests-Filippa/BestSellers_Tests.cs
@@ -0,0 +1,32 @@
+﻿
+using Project_Course_Submission.Services;
+
+namespace Project_Course_Submission.Tests.UnitTests;
+
+public class BestSellers_Tests
+{
+	[Fact]
+	public void TestGetBestSellers()
+	{
+		// Arrange
+		var bestSellersService = new BestSellersService();
+
+		// Act
+		var bestSellers = bestSellersService.GetBestSellers();
+		var bestItemsList = bestSellers.BestItems.ToList(); // Konvertera till en lista
+
+		// Assert
+		Assert.NotNull(bestSellers);
+		Assert.Equal("Best Sellers", bestSellers.Title);
+		Assert.Equal("View All", bestSellers.Ingress);
+		Assert.NotNull(bestItemsList);
+		Assert.Equal(7, bestItemsList.Count);
+		Assert.Equal("1", bestItemsList[0].Id);	
+		Assert.Equal("/images/bestsellers/270x295.svg", bestItemsList[0].ImageUrl);
+		Assert.Equal("Soft Chill Pants", bestItemsList[0].Title);
+		Assert.Equal("Stretchigt, mjukt material. Vikning i midjan, utsvängda byxben.", bestItemsList[0].Description);
+		Assert.Equal(40, bestItemsList[0].Price);
+	
+
+    }
+}
---
+++ UnitTests-Filippa/Categories_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Course_Submission.Migrations.Data;
+using Project_Course_Submission.Services;
+
+namespace Project_Course_Submission.Tests.UnitTests
+{
+	public class Categories_Tests
+	{
+		[Fact]
+		public void TestGetCategoryImagesId_HardcodedData()
+		{
+			// Arrange
+			var categoriesService = new CategoriesService();
+
+			// Act
+			var categoryImages = categoriesService.GetCategoryImagesId();
+
+			// Assert
+			
+			Assert.NotNull(categoryImages); // Kontrollera att resultatet inte är null
+			Assert.Equal(7, categoryImages.Count); // Kontrollera att det finns 7 kategori-bilder
+												   
+		}
+
+		[Fact]
+		public void TestGetCategoryImages_HardcodedData()
+		{
+			// Arrange
+			var categoriesService = new CategoriesService();
+
+			// Act
+			var categoryImages = categoriesService.GetCategoryImagesId();
+
+			// Assert
+			
+			Assert.NotNull(categoryImages); // Kontrollera att resultatet inte är null
+			 // Kontrollera att CategoryItems inte är null
+			Assert.Equal(7, categoryImages.Count); // Kontrollera att det finns 7 kategori-bilder i CategoryItems
+																		 
+		}
+	}
+}
